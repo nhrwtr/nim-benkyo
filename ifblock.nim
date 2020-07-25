de74a1e6d0d4f25b8fd73ecaf90adb27e3423b11
nomu-san <nhrwtr.21f98@gmail.com>
2020-07-25 10:16:01 +0900

    if statements

---
+++ ifblock.nim
@@ -0,0 +1,8 @@
+# if statements
+let name = readLine(stdin)
+if name == string.default:
+  echo "Poor soul, you lost your name?"
+elif name == "name":
+  echo "Very funny, your name is name."
+else:
+  echo "Hi, ", name, "!"
